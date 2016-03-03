b16679104787a9c5db982ec6133a995f5e47305f
Bhavin Patel <bhavin@bhavins-macbook-pro.local>
2016-03-03 02:12:42 -0800

    Initial Commit

---
+++ Acronym/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Acronym
+//
+//  Created by Bhavin Patel on 3/3/16.
+//  Copyright © 2016 Bhavin Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
